ca413a9d63e8ea30ebe43a8c651c1f02d17efa06
Chisco <contact@chisco.org>
2023-04-26 14:11:52 -0600

    title and desc stored in variables

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,9 +3,14 @@ import Card from "./Card";
 import LineChart from "./LineChart";
 
 function Dashboard() {
+  const cardTitle = "Your mood history";
+  const cardDesc =
+    "Ready to see your emotional rollercoaster?\
+     Hop on and let's take a ride!";
+
   return (
     <div id="Dashboard">
-      <Card title="Your mood history" description="">
+      <Card title={cardTitle} description={cardDesc}>
         <LineChart></LineChart>
       </Card>
     </div>
